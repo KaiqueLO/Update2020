291ec5543d20098f03d02a1bdb53f4db949fb516
Kaique Leite <kaique.lo32@gmail.com>
2020-04-26 20:46:52 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles web mvc App from c# Course";
+            ViewData["Name"] = "Kaique Leite Oliveira";
 
             return View();
         }
